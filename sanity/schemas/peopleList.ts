4943252f6599424e57deb1308b8c991ab08e13cb
Santiago Fuentes <santigf12@gmail.com>
2024-01-07 02:27:27 +0100

    add validation for people array

--- sanity/schemas/peopleList.ts
+++ sanity/schemas/peopleList.ts
@@ -27,4 +27,24 @@ export default {
             of: [{type: 'reference', to: {type: 'person'}}]
         }
     ],
+    validation: Rule => Rule.custom(fields => {
+        // Function to check for duplicates
+        const checkDuplicates = (array) => {
+            const unique = new Set(array?.map(item => item._ref));
+            return unique.size === array?.length;
+        };
+
+        // Check each field for duplicates
+        if (!checkDuplicates(fields.futured)) {
+            return 'You cannot have duplicate people in the featured list.';
+        }
+        if (!checkDuplicates(fields.testimonials)) {
+            return 'You cannot have duplicate people in the testimonials list.';
+        }
+        if (!checkDuplicates(fields.recommendations)) {
+            return 'You cannot have duplicate people in the recommendations list.';
+        }
+
+        return true;
+    })
 }
